6ba855cf4cfd39f2534da8de2cb77e6c4b8515b4
sandervano <ov5ander@gmail.com>
2022-01-16 14:23:13 +0100

    restructured files

--- examples/batterystats/Scripts/after_run.py
+++
@@ -1,5 +0,0 @@
-# noinspection PyUnusedLocal
-def main(device, *args, **kwargs):
-    device.shell('input tap 600 1000')  # Prevent the device from sleeping
-    device.clear_app_data('com.google.android.apps.maps')
-    pass
